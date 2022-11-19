88f3786f29f0c7b9940f2cb0e747d0b92cdfd34b
Alexey <alexey-nt@mail.ru>
2022-11-19 19:27:25 +0500

    dz

--- HomeWork к семинару 2/Program.cs
+++ HomeWork к семинару 2/Program.cs
@@ -5,8 +5,25 @@
 782 -> 8
 918 -> 1
 */
+/*
+int SecondDigit(int num)
+{
+    int num_2zn = num % 100;
+    int num_1zn = num_2zn / 10;
+    return num_1zn;
+}
 
+Console.Write("Введите ваше трехзначное число: ");
+int user_num = Convert.ToInt32(Console.ReadLine());
+while (user_num < 100 | user_num > 999)
+{
+    Console.Write("Ваше число не трехзначное.");
+    break;
+}
+int num2 = SecondDigit(user_num);
 
+Console.Write($"Вторая цифра вашего числа {num2}");
+*/
 
 /*
 Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
@@ -18,7 +35,22 @@
 32679 -> 6
 */
 
+int ThirdDigit(int num)
+{
+    if (num > 99)
+    {
+        int num1 = num / 100;
+        int num2 = num1 % 100000;
+        return num2;
+    }
+    else Console.WriteLine("Третьей цифры нет.");
+}
 
+Console.WriteLine("Введите ваше число: ");
+int user_num = Convert.ToInt32(Console.ReadLine());
+
+int num_1 = ThirdDigit(user_num);
+Console.Write($"Третья цифра вашего числа {num_1}");
 
 /*
 Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
